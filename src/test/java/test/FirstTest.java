26c46fd426f856750a650998854a13347f3a9cee
apostol <taraskorol1996@gmail.com>
2022-09-19 23:46:55 +0300

    new test

---
+++ src/test/java/test/FirstTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class FirstTest {
+
+
+   // @BeforeAll
+   //static void browserConfig() {
+    //    Configuration.browserSize = "1920x900";
+   // }
+
+    @Test
+    void fillFormTest() {
+
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("Oleg");
+        $("[id=userEmail]").setValue("oleg@pacan.com");
+        $("[id=currentAddress]").setValue("kiev");
+        $("[id=permanentAddress]").setValue("lviv");
+        $("#submit").scrollTo().click();
+
+        $("#output").shouldBe(visible);
+
+        $("#name").shouldHave(text("Oleg"));
+        $("#email").shouldHave(text("oleg@pacan.com"));
+        $("#currentAddress").shouldHave(text("kiev"));
+        $("#permanentAddress").shouldHave(text("lviv"));
+
+    }
+}
